3935aabb4519947453eb650996ec8bc7ff5b9c8d
sanderdebr <sander606@gmail.com>
2019-12-05 19:30:34 +0100

    update

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@ parcel build index.html --public-url ./
 git subtree push --prefix dist origin gh-pages
  */
 
+require('./animations.js');
+require('./cursor.js');
+require('./scroll.js');
+
 window.addEventListener('load', (event) => {
     console.log("Time until everything loaded: ", Date.now()-timerStart, 'ms');
 });
+
